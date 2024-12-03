51428f41ed33276e39d8f8fac2aabb4af4244372
Acoronag <acoronag@calpoly.edu>
2024-12-02 17:31:37 -0800

    h

--- hw2.py
+++ hw2.py
@@ -1,20 +1,91 @@
+#from curses.textpad import rectangle
+
 import data
 
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def create_rectangle(point1,point2):
+    if point1.x < point2.x and point1.y > point2.y:
+        return  data.Rectangle(point1,point2)
+    elif point1.x > point2.x and point1.y < point2.y:
+        return data.Rectangle(point2, point1)
+    elif point1.x > point2.x and point1.y > point2.y:
+        return data.Rectangle(data.Point(point2.x, point1.y), data.Point(point1.x, point2.y))
+    else:
+        return data.Rectangle(data.Point(point1.x, point2.y), data.Point(point2.x, point1.y))
 
 # Part 2
-
+def shorter_duration_than(duration1, duration2):
+    if duration1.minutes < duration2.minutes:
+        return True
+    elif duration1.minutes > duration2.minutes:
+        return False
+    else:
+        if duration1.seconds < duration2.seconds:
+            return True
+        else:
+            return False
 
 # Part 3
-
+def songs_shorter_than(lista, duration):
+    listb = []
+    for song in lista:
+        if song.duration.minutes < duration.minutes:
+            listb.append(song)
+        elif song.duration.minutes == duration.minutes and song.duration.seconds < duration.seconds:
+            listb.append(song)
+    return listb
 
 # Part 4
-
+def running_time(lista, listb):
+    sum = data.Duration(0,0)
+    for idx in listb:
+        sum = data.Duration(sum.minutes + lista[idx].duration.minutes, sum.seconds + lista[idx].duration.seconds)
+    while sum.seconds > 60:
+        sum.seconds -= 60
+        sum.minutes += 1
+    return sum
 
 # Part 5
-
+def valid_route(links, route):
+    connection = 0
+    for i in range(len(route)-1):
+        for j in range(len(links)):
+            if (route[i] == links[j][0] or route[i] == links[j][1]) and (route[i+1] == links[j][0] or route[i+1] == links[j][1]):
+                connection += 1
+                break
+    if connection == len(route)-1:
+        return True
+    else:
+        return False
 
 # Part 6
+
+def longest_repetition(lista):
+    if lista == []:
+        return None
+    best_streak = 1
+    best_idx = 0
+    for i in range(len(lista)-1):
+        if lista[i] == lista[i+1]:
+            best_streak += 1
+        else:
+            new_idx = i + 1
+            break
+    while new_idx != len(lista)-1:
+        streak1 = 1
+        temp_idx = new_idx
+        for i in range(new_idx,len(lista)-1):
+            if lista[i] == lista[i+1]:
+                streak1 += 1
+            else:
+                break
+        if best_streak < streak1:
+            best_streak = streak1
+            best_idx = temp_idx
+        new_idx = i + 1
+    return best_idx
+
+
+
--- hw2_tests.py
+++ hw2_tests.py
@@ -7,22 +7,117 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_create_rectangle_1(self):
+        point1 = data.Point(4,6)
+        point2 = data.Point(6,8)
+        result = hw2.create_rectangle(point1,point2)
+        expected = data.Rectangle(data.Point(4,8),data.Point(6,6))
+        self.assertEqual(expected,result)
 
+    def test_create_rectangle_2(self):
+        point1 = data.Point(6,8)
+        point2 = data.Point(-2,-8)
+        result = hw2.create_rectangle(point1,point2)
+        expected = data.Rectangle(data.Point(-2,8),data.Point(6,-8))
+        self.assertEqual(expected,result)
 
     # Part 2
+    def test_shorter_duration_than_1(self):
+        duration1 = data.Duration(4,50)
+        duration2 = data.Duration(4,50)
+        result = hw2.shorter_duration_than(duration1,duration2)
+        expected = False
+        self.assertEqual(result,expected)
 
+    def test_shorter_duration_than_2(self):
+        duration1 = data.Duration(4,50)
+        duration2 = data.Duration(4,20)
+        result = hw2.shorter_duration_than(duration1,duration2)
+        expected = False
+        self.assertEqual(result,expected)
+
+    def test_shorter_duration_than_3(self):
+        duration1 = data.Duration(4, 20)
+        duration2 = data.Duration(4, 50)
+        result = hw2.shorter_duration_than(duration1, duration2)
+        expected = True
+        self.assertEqual(result, expected)
 
     # Part 3
+    def test_songs_shorter_than_1(self):
+        song1 = data.Song("Marshmellow","Alone", data.Duration(2,30))
+        song2 = data.Song("Jake Paul","Its Everyday Bro", data.Duration(1,59))
+        song3 = data.Song("KSI","Thick of it", data.Duration(2,15))
+        lista = [song1,song2,song3]
+        result = hw2.songs_shorter_than(lista,data.Duration(2,20))
+        expected = [song2,song3]
+        self.assertEqual(expected,result)
 
+    def test_songs_shorter_than_2(self):
+        song1 = data.Song("Marshmellow","Alone", data.Duration(1,30))
+        song2 = data.Song("Jake Paul","Its Everyday Bro", data.Duration(1,9))
+        song3 = data.Song("KSI","Thick of it", data.Duration(2,15))
+        lista = [song1,song2,song3]
+        result = hw2.songs_shorter_than(lista,data.Duration(1,50))
+        expected = [song1,song2]
+        self.assertEqual(expected,result)
 
     # Part 4
+    def test_running_time_1(self):
+        song1 = data.Song("Marshmellow","Alone", data.Duration(1,30))
+        song2 = data.Song("Jake Paul","Its Everyday Bro", data.Duration(1,10))
+        song3 = data.Song("KSI","Thick of it", data.Duration(2,15))
+        lista = [song1,song2,song3]
+        listb = [0,0,2,1]
+        result = hw2.running_time(lista,listb)
+        expected = data.Duration(6,25)
+        self.assertEqual(expected,result)
+
+    def test_running_time_2(self):
+        song1 = data.Song("Marshmellow","Alone", data.Duration(1,30))
+        song2 = data.Song("Jake Paul","Its Everyday Bro", data.Duration(1,10))
+        song3 = data.Song("KSI","Thick of it", data.Duration(2,15))
+        lista = [song1,song2,song3]
+        listb = [0,0,2,1,2,2,2,1]
+        result = hw2.running_time(lista,listb)
+        expected = data.Duration(14,20)
+        self.assertEqual(expected,result)
 
 
     # Part 5
+    def test_valid_route_1(self):
+        lista = [["stockton","lathrop"],["stockton","tracy"],["tracy","acampo"],["acampo",'lodi']]
+        listb = ["stockton","tracy","acampo","lodi"]
+        result = hw2.valid_route(lista,listb)
+        expected = True
+        self.assertEqual(expected,result)
 
+    def test_valid_route_1(self):
+        lista = [["stockton","lathrop"],["stockton","tracy"],["tracy","acampo"],["acampo",'lodi']]
+        listb = ["tracy","stockton","lathrop"]
+        result = hw2.valid_route(lista,listb)
+        expected = True
+        self.assertEqual(expected,result)
+
+    def test_valid_route_1(self):
+        lista = [["stockton","lathrop"],["stockton","tracy"],["tracy","acampo"],["acampo",'lodi']]
+        listb = ["lodi","tracy","acampo"]
+        result = hw2.valid_route(lista,listb)
+        expected = False
+        self.assertEqual(expected,result)
 
     # Part 6
+    def test_longest_repitition_1(self):
+        lista = [2,2,2,3,3,3,4,4,4,4,5,5,5,5]
+        result = hw2.longest_repetition(lista)
+        expected = 6
+        self.assertEqual(expected,result)
 
+    def test_longest_repitition_2(self):
+        lista = [1,2,2,3,3,3,5,5,5,5,5,4,4,4,4]
+        result = hw2.longest_repetition(lista)
+        expected = 6
+        self.assertEqual(expected,result)
 
 
 
